9be013cb8d0146b0e4c3a532e8d10bdbe6769ba2
AMR IBRAHIM AHMED <amrzain93@icloud.com>
2023-03-16 14:29:30 -0700

    4-puts.c fix 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /**
-main -Entry point
-Description: A C program that prints that puts function
-Return : Alawys 0  (success)
- */
+* main - A program that prints a line with puts function
+* Return : 0 (success)
+*/
 
 
 int main(void)
 {
-	  puts("\"Programming is like building a multilingual puzzle");
-	  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
