d2eeaf19539cc9e9fe7165e682a9932b136a31ca
AMR IBRAHIM AHMED <amrzain93@icloud.com>
2023-03-24 14:11:37 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Multiply 2 int.
+ *
+ * @a: first int.
+ * @b: second int.
+ *
+ * Return: Addition of the multiplicattion.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
